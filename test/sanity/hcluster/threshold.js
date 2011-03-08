61065e931b8b7670ad3c99d5f68d515816aaf512
Heather Arthur <fayearthur@gmail.com>
2011-03-08 11:13:14 -0800

    clean up 'key' and 'index' metadata from JSON after clustering

--- test/sanity/hcluster/threshold.js
+++ test/sanity/hcluster/threshold.js
@@ -15,9 +15,9 @@ var colors = [
 ];
 
 var clusters = clusterfck.hcluster(colors, clusterfck.EUCLIDEAN_DISTANCE, clusterfck.AVERAGE_LINKAGE, 0);
-var expected = [{"canonical":[255,255,255],"key":0,"index":0,"size":1},{"canonical":[253,253,253],"key":1,"index":1,"size":1},{"canonical":[120,130,255],"key":2,"index":2,"size":1},{"canonical":[120,130,40],"key":3,"index":3,"size":1},{"canonical":[23,30,10],"key":4,"index":4,"size":1},{"canonical":[250,255,246],"key":5,"index":5,"size":1},{"canonical":[255,255,245],"key":6,"index":6,"size":1},{"canonical":[31,30,12],"key":7,"index":7,"size":1},{"canonical":[14,20,1],"key":8,"index":8,"size":1}];
+var expected = [{"canonical":[255,255,255],"size":1},{"canonical":[253,253,253],"size":1},{"canonical":[120,130,255],"size":1},{"canonical":[120,130,40],"size":1},{"canonical":[23,30,10],"size":1},{"canonical":[250,255,246],"size":1},{"canonical":[255,255,245],"size":1},{"canonical":[31,30,12],"size":1},{"canonical":[14,20,1],"size":1}];
 assert.deepEqual(clusters, expected, "clustering with threshold 0 should do nothing");
 
 var clusters = clusterfck.hcluster(colors, clusterfck.EUCLIDEAN_DISTANCE, clusterfck.AVERAGE_LINKAGE, 5);
-var expected = [{"canonical":[253,253,253],"left":{"canonical":[253,253,253],"key":1,"index":1,"size":1},"right":{"canonical":[255,255,255],"key":0,"index":0,"size":1},"key":1,"size":2,"index":0},{"canonical":[120,130,255],"key":2,"index":1,"size":1},{"canonical":[120,130,40],"key":3,"index":2,"size":1},{"canonical":[23,30,10],"key":4,"index":3,"size":1},{"canonical":[250,255,246],"key":5,"index":4,"size":1},{"canonical":[255,255,245],"key":6,"index":5,"size":1},{"canonical":[31,30,12],"key":7,"index":6,"size":1},{"canonical":[14,20,1],"key":8,"index":7,"size":1}];
+var expected = [{"canonical":[253,253,253],"left":{"canonical":[253,253,253],"size":1},"right":{"canonical":[255,255,255],"size":1},"size":2},{"canonical":[120,130,255],"size":1},{"canonical":[120,130,40],"size":1},{"canonical":[23,30,10],"size":1},{"canonical":[250,255,246],"size":1},{"canonical":[255,255,245],"size":1},{"canonical":[31,30,12],"size":1},{"canonical":[14,20,1],"size":1}]
 assert.deepEqual(clusters, expected, "clustering with threshold 5 should only cluster two items");
